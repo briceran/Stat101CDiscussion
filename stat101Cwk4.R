bec80eeaa08f2c79e2b1a23b2329cac7440baa35
Brice Randolph <brandolph@ucla.edu>
2016-04-19 10:12:41 -0700

    Added files via upload

---
+++ stat101Cwk4.R
@@ -0,0 +1,93 @@
+# Stat101C Week 4
+# Brice Randolph
+# UCLA Statistics
+
+x<-seq(0,200)
+# Transformations and complex numbers
+read.csv(na.strings = )
+
+
+floor((x-min(x))/range(x)*screenWidth)
+
+
+# write pseudocode that would generate a square at
+# point (x,y)
+
+sys
+
+# write a function that takes as an argument a complex
+# number Z.  Here, Z should be an object with two 
+# arguments, (i,j), where i corresponds to the 
+# imaginary part and j correspongs to the complex part
+# plot values using ggplot2
+
+# 1. learn to create random events 
+
+# 2. learn to create queues of people entering a line
+# avg # people coming per day and average service time
+
+# 3. learn to code simulations 
+
+# simulate the Diddy Riese line:  most realistic
+# simulation wins.  poisson? exponential?
+
+
+
+## simulate dating game 
+
+
+# write helper functions to help tinder out
+# could their matching algorithm improve?
+
+
+
+
+# Data generating process ## DGP
+# Compilation of common speech patterns of Lebowski
+words<-rep("Man",4)
+words<-c(words,rep("Relax, man",30),rep("theDude",7),rep("dude",54))
+words<-c(words,rep("White-Russian",8),"It was an expensive 
+         rug, man.","Yeah, well, you know, that's just, like, your opinion, man.",
+         "Let me explain something to you. Um, I am not Mr. Lebowski. You're Mr. Lebowski. I'm the Dude. So that's what you call me. You know, that or, uh, His Dudeness, or uh, Duder, or El Duderino if you're not into the whole brevity thing.")         
+words<-sample(x = words,replace = FALSE,size = length(words))
+words
+
+
+
+
+
+# standardize the font in words call the variable: 
+
+
+# what's the mode?
+
+
+domains<-data.frame(x=seq(0,1,by = 0.1),y =seq(0,10,by=1))
+domains
+
+
+# write function get_max_length:
+
+
+# DataFest
+
+# have someone on your team learn regular expressions
+
+# have someone learn qplot, have them teach it
+# to the group.
+
+# Use this same process for all of these.
+# have someone learn a few ML/learning algorithms
+# ie glm,random forest, lasso - Stanford machine learning
+# Datacamp would be really useful- plug1
+
+# who's the most social - let them get the most sleep
+# the night before and lead the presentation
+
+
+# dont over optimize - wait till the end to implement
+# anything crazy
+
+# COMMENT YOUR CODE - google python style guide 
+
+
